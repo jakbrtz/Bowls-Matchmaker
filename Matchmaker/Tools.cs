bc23faec87965d1b46075b149780e78543d6a356
Jake <jake@bortz.com.au>
2021-01-30 12:02:55 +1100

    Ignore apostrophes and dashes when searching

--- Matchmaker/Tools.cs
+++ Matchmaker/Tools.cs
@@ -21,21 +21,28 @@ namespace Matchmaker
             return (players.Count + 1).ToString();
         }
 
+        static string SimplifySearch(string old) => old.ToLower().Replace("'", "").Replace("-", "").Replace("'", "");
+
         public static bool Filter(Player player, string search)
         {
             if (string.IsNullOrEmpty(search)) return true;
-            return player.Name.IndexOf(search, StringComparison.OrdinalIgnoreCase) != -1 || player.TagNumber?.ToString().Contains(search) == true;
+            return SimplifySearch(player.Name).IndexOf(SimplifySearch(search), StringComparison.OrdinalIgnoreCase) != -1 || player.TagNumber?.ToString().Contains(search) == true;
         }
 
         public static int RelevanceToSearch(Player player, string search)
         {
+            if (search == player.TagNumber) return int.MinValue;
+
+            string playerName = SimplifySearch(player.Name);
+            search = SimplifySearch(search);
+
             const int bignumber = 1 << 10; // there's no way the user will use more than this many words
-            if (player.Name == search) return int.MinValue;
+            if (playerName == search) return int.MinValue;
             int earliestMatchingWord = -1;
             int numberOfMatchingWords = 0;
             foreach (string searchWord in search.Split(' '))
             {
-                var names = player.Name.Split(' ');
+                var names = playerName.Split(' ');
                 for (int i = 0; i < names.Length; i++)
                 {
                     if (names[i].StartsWith(searchWord, StringComparison.OrdinalIgnoreCase))

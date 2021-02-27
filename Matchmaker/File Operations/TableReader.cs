01220947b0c550abbbd5a9aed84b6827854ffcb9
Jake <jake@bortz.com.au>
2021-02-27 18:43:09 +1100

    Split up ReadWriteTable.cs

---
+++ Matchmaker/File Operations/TableReader.cs
@@ -0,0 +1,74 @@
+﻿using ClosedXML.Excel;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace Matchmaker.FileOperations
+{
+    public interface ITableReader : IDisposable
+    {
+        bool ReadRow(out string[] values);
+        int NumColumns { get; }
+    }
+
+    public class ExcelReader : ITableReader
+    {
+        readonly XLWorkbook workbook;
+        readonly IXLWorksheet worksheet;
+
+        readonly int totalRows;
+        readonly int totalColumns;
+        int row = 0;
+
+        public ExcelReader(string filename)
+        {
+            workbook = new XLWorkbook(File.Open(filename, FileMode.Open, FileAccess.Read, FileShare.ReadWrite));
+            worksheet = workbook.Worksheets.First();
+
+            totalRows = worksheet.LastRowUsed().RowNumber();
+            totalColumns = worksheet.LastColumnUsed().ColumnNumber();
+        }
+
+        public bool ReadRow(out string[] values)
+        {
+            values = new string[totalColumns];
+            if (row >= totalRows)
+                return false;
+            for (int i = 0; i < totalColumns; i++)
+                values[i] = worksheet.Cell(row + 1, i + 1).Value.ToString();
+            row++;
+            return true;
+        }
+
+        public int NumColumns => totalColumns;
+
+        public void Dispose()
+        {
+            workbook.Dispose();
+        }
+    }
+
+    class SingleColumnReader : ITableReader
+    {
+        readonly StreamReader streamReader;
+
+        public SingleColumnReader(string filename)
+        {
+            streamReader = new StreamReader(filename);
+        }
+
+        public bool ReadRow(out string[] values)
+        {
+            string line = streamReader.ReadLine();
+            values = new string[] { line };
+            return line != null;
+        }
+
+        public int NumColumns => 1;
+
+        public void Dispose()
+        {
+            streamReader.Dispose();
+        }
+    }
+}
---
+++ Matchmaker/File Operations/TableWriter.cs
@@ -0,0 +1,62 @@
+﻿using ClosedXML.Excel;
+using System;
+using System.IO;
+
+namespace Matchmaker.FileOperations
+{
+    interface ITableWriter : IDisposable
+    {
+        void AddRow(params string[] values);
+    }
+
+    class ExcelWriter : ITableWriter
+    {
+        readonly string filename;
+        readonly XLWorkbook workbook;
+        readonly IXLWorksheet worksheet;
+
+        int row = 0;
+
+        public ExcelWriter(string filename)
+        {
+            this.filename = filename;
+            workbook = new XLWorkbook();
+            worksheet = workbook.Worksheets.Add("Sheet1");
+        }
+
+        public void AddRow(params string[] values)
+        {
+            for (int i = 0; i < values.Length; i++)
+                worksheet.Cell(row + 1, i + 1).Value = values[i];
+            row++;
+        }
+
+        public void Dispose()
+        {
+            worksheet.Columns().AdjustToContents();
+            workbook.SaveAs(filename);
+            workbook.Dispose();
+        }
+    }
+
+    class SingleColumnWriter : ITableWriter
+    {
+        readonly StreamWriter streamWriter;
+
+        public SingleColumnWriter(string filename)
+        {
+            streamWriter = new StreamWriter(filename);
+        }
+
+        public void AddRow(params string[] values)
+        {
+            if (values.Length != 1) throw new ArgumentException("SingleColumnWriter is only allowed to write one value per line");
+            streamWriter.WriteLine(values[0]);
+        }
+
+        public void Dispose()
+        {
+            streamWriter.Dispose();
+        }
+    }
+}

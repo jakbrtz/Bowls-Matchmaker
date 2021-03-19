2bfecb7cd4eda826065d75b0bbf2546a41d71c1d
Jake <jake@bortz.com.au>
2021-03-19 22:59:02 +1100

    Moved PairOfPlayers out of Penalties

---
+++ Matchmaker/Algorithms/Structures/PairOfPlayers.cs
@@ -0,0 +1,30 @@
+﻿using Matchmaker.Data;
+
+namespace Matchmaker.Algorithms.Structures
+{
+    public struct PairOfPlayers
+    {
+        public Player player1;
+        public Player player2;
+
+        public PairOfPlayers(Player player1, Player player2)
+        {
+            this.player1 = player1;
+            this.player2 = player2;
+        }
+
+        public override int GetHashCode()
+        {
+            return player1?.GetHashCode() ^ player2?.GetHashCode() ?? 0;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is PairOfPlayers other &&
+                ((player1 == other.player1 && player2 == other.player2) ||
+                 (player1 == other.player2 && player2 == other.player1));
+        }
+
+        public override string ToString() => player1 + " " + player2;
+    }
+}
--- Matchmaker/Data/Penalties.cs
+++ Matchmaker/Data/Penalties.cs
@@ -1,31 +1,5 @@
 ﻿namespace Matchmaker.Data
 {
-    public struct PairOfPlayers
-    {
-        public Player player1;
-        public Player player2;
-
-        public PairOfPlayers(Player player1, Player player2)
-        {
-            this.player1 = player1;
-            this.player2 = player2;
-        }
-
-        public override int GetHashCode()
-        {
-            return player1?.GetHashCode() ^ player2?.GetHashCode() ?? 0;
-        }
-
-        public override bool Equals(object obj)
-        {
-            return obj is PairOfPlayers other && 
-                ((player1 == other.player1 && player2 == other.player2) ||
-                 (player1 == other.player2 && player2 == other.player1));
-        }
-
-        public override string ToString() => player1 + " " + player2;
-    }
-
     public abstract class Penalty
     {
         public HistoryOfPenalty historical = new HistoryOfPenalty();
